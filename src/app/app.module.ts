fa91674aed7f1204b6d3bbf5f2de46257404bf25
Matthew Davis <matthew@appsoa.io>
2018-05-13 11:47:26 -0500

    bump

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {MaterialModule} from './shared/MaterialModule';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { PageHeaderComponent } from './page-header/page-header.component';
 
 @NgModule({
 
     declarations: [
 
-        AppComponent
+        AppComponent,
+
+        PageHeaderComponent
 
     ],
 
